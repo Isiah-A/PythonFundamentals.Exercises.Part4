95585e2ff53558bd0f4a60c11ab0b290d83e006f
Isiah-A <isiaha478@gmail.com>
2025-03-12 11:59:05 -0400

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,12 @@
+for x in range(1, 101, 1): #start at 1, finish at 100, go up by 1
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+
+
+
